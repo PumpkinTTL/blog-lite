a5eabfed4812d477a2da3c29856827d21988df34
Pumpkin <PumpkinTTL@163.com>
2026-03-18 18:06:39 +0800

    update:更新首页UI-备份

--- src/components/v1/home/HomeSidebar.vue
+++ src/components/v1/home/HomeSidebar.vue
@@ -4,10 +4,7 @@
     <AdContainer type="glass">
       <iframe
         src="https://static.nube.sh/html-video/speed-sc/1280x720/index.html?lp=https%3A%2F%2Fnube.sh%2Finvite%2F6723572561HYYC"
-        style="width: 100%; height: auto; border: none"
-        scrolling="no"
-        frameborder="0"
-      >
+        style="width: 100%; height: auto; border: none" scrolling="no" frameborder="0">
       </iframe>
     </AdContainer>
 
@@ -26,10 +23,7 @@
             <span class="task-count">{{ task.done }}/{{ task.total }}</span>
           </div>
           <div class="progress-track">
-            <div
-              class="progress-fill"
-              :style="{ width: `${(task.done / task.total) * 100}%` }"
-            ></div>
+            <div class="progress-fill" :style="{ width: `${(task.done / task.total) * 100}%` }"></div>
           </div>
         </li>
       </ul>
@@ -40,13 +34,8 @@
     </section>
 
     <!-- Second Ad Container (Improved Light Style) -->
-    <AdContainer
-      title="加入技术沙龙"
-      description="与行业大佬零距离接触，探索最前沿的技术变迁与工程实践。"
-      buttonText="立即报名"
-      icon="users-rectangle"
-      type="default"
-    />
+    <AdContainer title="加入技术沙龙" description="与行业大佬零距离接触，探索最前沿的技术变迁与工程实践。" buttonText="立即报名" icon="users-rectangle"
+      type="default" />
   </aside>
 </template>
 
